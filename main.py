3fab903c9a167c21aad910fc706a184749a60d8c
Dhairya <dhairyasaraswat29@gmail.com>
2022-12-08 18:25:57 +0530

    Pandas---

--- main.py
+++ main.py
@@ -11,6 +11,7 @@
 
 import pandas as pd
 import numpy as np
+
 """
 # -------------------Series-------------------------------
 # Series is a one dimensional labeled array capable of holding data of any type.
@@ -278,4 +279,192 @@ print(rc1)
 # print(ri1)
 # This is removed from python pandas.
 print("-----------------------------------------------")
+
+
+# Boolean Indexing -
+# In boolean indexing, we will select subsets of data based on the actual values of
+# the data in the DataFrame and not on their row/column labels or integer locations.
+
+dict = {'name': ["A", "B", "C", "D"],
+        'score': [90, 85, 80, 98]
+        }
+df = pd.DataFrame(dict, index=[True, False, True, False])
+
+print(df)
+
+# Accessing a Dataframe with a boolean index using .loc[]
+
+# In order to access a dataframe with a boolean index using .loc[],
+# simply pass a boolean value (True or False) in a .loc[] function.
+
+print("Boolean index by loc -")
+print(df.loc[True])
+
+# Accessing a Dataframe with a boolean index using .iloc[]
+# In order to access a dataframe using .iloc[], we have to pass a boolean value (True or False)
+# but iloc[] function accepts only integer as an argument so it will throw an error
+# Example -
+
+# print("Boolean index by iloc -")
+# print(df.iloc[True])
+# print("It will show TypeError as only integers value can be passed.")
+
+print("Correct method by iloc is by passing integer -")
+print(df.iloc[0])
+print(df.iloc[1])
+print(df.iloc[2])
+print(df.iloc[3])
+
+# Applying boolean mask to a dataframe -
+
+print("----------------------------------------")
+tb = pd.read_csv('table.csv')
+
+dt = pd.DataFrame(tb, index=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+print(dt[[True, False, True, False, True, False, True, False, True, False]])
+
+# Masking data based on column value
+# In a dataframe we can filter a data based on a column value.
+# In order to filter data, we can apply certain conditions on the dataframe using different operators
+# like ==, >, <, <=, >=. When we apply these operators to the dataframe then it produces a Series of True and False.
+print("---------------------------")
+print(df['name'] == 'C')
+print("Ex-2")
+data = pd.read_csv("nba.csv", index_col="Name")
+print(data['Age'] > 25)
+print(data['Age'] == 29)
+
+
+# ------------------------------Group By ---------------------------------
+# creating a grouping of categories and apply a function to the categories.
+# Splitting : It is a process in which we split data into group by applying some conditions on datasets.
+# Applying : It is a process in which we apply a function to each group independently
+# Combining : It is a process in which different datasets are combined after applying groupby
+
+# Splitting data into groups -
+# Grouping data with one key
+
+data1 = {'Name':['Jai', 'Anuj', 'Jai', 'Prince',
+                 'Gaurav', 'Anuj', 'Prince', 'Abhi'],
+        'Age':[27, 24, 22, 32,
+               33, 36, 27, 32],
+        'Address':['Nagpur', 'Kanpur', 'Allahabad', 'Kannuaj',
+                   'Jaunpur', 'Kanpur', 'Allahabad', 'Aligarh'],
+        'Qualification':['Msc', 'MA', 'MCA', 'Phd',
+                         'B.Tech', 'B.com', 'Msc', 'MA'],
+         'Score': [23, 34, 35, 45, 47, 50, 52, 53]}
+
+df = pd.DataFrame(data1)
+# --
+gn = df.groupby('Name')
+print(df.groupby('Name').groups)  # Giving index
+print(gn.first())
+
+# Grouping data with multiple keys -
+print("\nGrouping by multiple keys -")
+asq = df.groupby(['Name', 'Qualification'])
+print(asq.groups)
+print(asq.first())
+
+# Grouping data by sorting keys
+# Group keys are sorted by default using the groupby operation.
+# print("----------------------")
+# print(df.groupby(['Name']).sum())
+# print(df.groupby(['Name'], sort=False).sum())
+
+# -------------------Iterating through groups---------------------------
+print("Iteration -\n")
+g = df.groupby('Name')
+for name, group in g:
+        print(name)
+        print(group)
+        print("")
+
+# -------------------Selecting a group----------------------------------
+# get_group('__') - Selecting a particular group among many groups
+print("Selecting a group -")
+g1 = df.groupby('Name')
+print(g1.get_group('Prince'))
+g2 = df.groupby(['Name', 'Qualification'])
+print(g2.get_group(('Anuj', 'MA')))
+
+
+# -------------------Applying function-----------------------------------
+# Aggregation -
+print("\nApplying functions- ")
+grp = df.groupby('Name')
+nm = grp.aggregate(np.sum)
+print(nm)
+# Multiple Functions -
+print(grp['Age'].agg([np.sum, np.mean]))
+print("\n", grp.agg({'Age':'sum', 'Score':'mean'}))
+
+
+# Pandas Merging, Joining and Concatenating
+#  --1--Concatenating DataFrame using .concat()
+# concat() function this function concat a dataframe and returns a new dataframe.
+d1 = {"Class":["1", "2"],
+      "Section":["A", "B"]}
+d2 = {"Class":["3", "4"],
+      "Section":["C", "D"]}
+df1 = pd.DataFrame(d1, index=[0, 1])
+df2 = pd.DataFrame(d2, index=[2, 3])
+print("Dataframe 1\n", df1, "\nDataFrame 2\n", df2)
+print("After concatenation-")
+print(pd.concat([df1, df2]))
+
+# Concatenating DataFrame with group keys
+print("Group keys -")
+print(pd.concat([df1, df2], keys=['K1', 'K2']))
+
+# Concatenating DataFrame by setting logic on axes
+df1 = pd.DataFrame(d1, index=[0, 1])
+df2 = pd.DataFrame(d2, index=[1, 2])
+print("Dataframe 1\n", df1, "\nDataFrame 2\n", df2)
+print("After concatenation by logic axes-")
+print(pd.concat([df1, df2], axis=0, join='inner'))
+print("----")
+print(pd.concat([df1, df2], axis=1, join='inner'))
+print("----")
+# print(pd.concat([df1, df2], axis=1, sort=False))
+print(pd.concat([df1, df2], axis=1, sort=False))
+
+# Concatenating DataFrame using .append()
+print("Appending -")
+print(df1.append(df2))
+
+# Concatenating DataFrame by ignoring indexes :
+df3 = pd.DataFrame(d1, index=[0, 1])
+df4 = pd.DataFrame(d2, index=[0, 1])
+print("Result shown by ignoring indexes-")
+print(pd.concat([df3, df4], ignore_index=True))
+
+#------------------
+
+data1 = {'key': ['K0', 'K1', 'K2', 'K3'],
+         'Name':['Jai', 'Princi', 'Gaurav', 'Anuj'], 
+        'Age':[27, 24, 22, 32],} 
+    
+data2 = {'key': ['K0', 'K1', 'K2', 'K3'],
+         'Address':['Nagpur', 'Kanpur', 'Allahabad', 'Kannuaj'], 
+        'Qualification':['Btech', 'B.A', 'Bcom', 'B.hons']} 
+ 
+df = pd.DataFrame(data1)
+  
+df1 = pd.DataFrame(data2) 
+  
+print(df, "\n", df1) 
+
+print(pd.merge(df, df1, on='key'))
 """
+
+
+
+
+
+
+
+
+
+
